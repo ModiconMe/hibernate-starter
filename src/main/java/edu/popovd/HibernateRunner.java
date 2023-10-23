f0d4452737309a2676392e8e73456a641a2ee33b
modiconme <rebeelsc@gmail.com>
2023-10-23 19:57:09 +0700

    1. hibernate config

---
+++ src/main/java/edu/popovd/HibernateRunner.java
@@ -0,0 +1,33 @@
+package edu.popovd;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.concurrent.BlockingDeque;
+
+public class HibernateRunner {
+    public static void main(String[] args) throws SQLException {
+//        BlockingDeque<Connection> pool = null;
+//        Connection connection = pool.take();
+//        SessionFactory
+
+//        DriverManager.getConnection(
+//                "db.url",
+//                "db.username",
+//                "db.password");
+//        Session
+
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
+             Session session = sessionFactory.openSession()
+        ) {
+            System.out.println("OK");
+        }
+    }
+}
